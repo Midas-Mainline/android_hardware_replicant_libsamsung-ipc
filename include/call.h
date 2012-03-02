bcdc98b0c964a92cb3fc956e9f84a78b5f08f00e
Simon Busch <morphis@gravedo.de>
2012-03-02 19:12:22 +0100

    call: add pack method for dtmf burst send message

--- include/call.h
+++ include/call.h
@@ -83,6 +83,7 @@ void ipc_call_outgoing_setup(struct ipc_call_outgoing *message, unsigned char ty
 unsigned int ipc_call_list_response_get_num_entries(struct ipc_message_info *response);
 struct ipc_call_list_entry* ipc_call_list_response_get_entry(struct ipc_message_info *response, unsigned int num);
 char* ipc_call_list_response_get_entry_number(struct ipc_message_info *response, unsigned int num);
+unsigned char* ipc_call_cont_dtmf_burst_pack(struct ipc_call_cont_dtmf *message, unsigned char *burst, int burst_len);
 
 #endif
 
--- samsung-ipc/call.c
+++ samsung-ipc/call.c
@@ -113,4 +113,22 @@ char* ipc_call_list_response_get_entry_number(struct ipc_message_info *response,
     return number;
 }
 
+unsigned char* ipc_call_cont_dtmf_burst_pack(struct ipc_call_cont_dtmf *message, unsigned char *burst, int burst_len)
+{
+    unsigned char *data = NULL;
+    int data_len = sizeof(struct ipc_call_cont_dtmf) + burst_len;
+
+    assert(message != 0);
+    assert(burst != 0);
+    assert(burst_len != 0);
+
+    data = (unsigned char*) malloc(sizeof(unsigned char) * data_len);
+    memset(data, 0, data_len);
+
+    memcpy(data, message, sizeof(struct ipc_call_cont_dtmf));
+    memcpy(data + sizeof(struct ipc_call_cont_dtmf), burst, burst_len);
+
+    return data;
+}
+
 // vim:ts=4:sw=4:expandtab
