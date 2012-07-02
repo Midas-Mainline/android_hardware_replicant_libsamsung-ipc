ec0da948c69d1e8aea8dcdbbe2e4552591f18b47
Alexander Tarasikov <alexander.tarasikov@gmail.com>
2012-07-02 21:11:17 +0400

    Change i9250 and i9100 to lowercase to fix runtime detection

--- samsung-ipc/ipc_devices.c
+++ samsung-ipc/ipc_devices.c
@@ -24,8 +24,8 @@
 
 struct ipc_device_desc ipc_devices[] = {
     { "galaxys2", "i9100",  &xmm6260_default_handlers,   &xmm6260_i9100_fmt_ops, &xmm6260_rfs_ops },
-    { "galaxys2", "SMDK4210",  &xmm6260_default_handlers,    &xmm6260_i9100_fmt_ops,  &xmm6260_rfs_ops },
-    { "maguro", "Tuna",  &xmm6260_default_handlers,    &xmm6260_i9250_fmt_ops,    &xmm6260_rfs_ops },
+    { "galaxys2", "smdk4210",  &xmm6260_default_handlers,    &xmm6260_i9100_fmt_ops,  &xmm6260_rfs_ops },
+    { "maguro", "tuna",  &xmm6260_default_handlers,    &xmm6260_i9250_fmt_ops,    &xmm6260_rfs_ops },
     { "crespo", "herring",  &crespo_default_handlers,   &crespo_fmt_ops,    &crespo_rfs_ops },
     { "aries",  "aries",    &aries_default_handlers,    &aries_fmt_ops,     &aries_rfs_ops },
     { "aries",  "gt-p1000", &aries_default_handlers,    &aries_fmt_ops,     &aries_rfs_ops }
