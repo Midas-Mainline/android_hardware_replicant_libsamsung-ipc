94ca3db63b985c7ad7696f3bce50555950e1c17a
Simon Busch <morphis@gravedo.de>
2012-08-24 07:25:19 +0200

    Add identifiable named header file which should be the only one used by clients

---
+++ include/samsung-ipc.h
@@ -0,0 +1,28 @@
+/**
+ * This file is part of libsamsung-ipc.
+ *
+ * Copyright (C) 2012 Simon Busch <morphis@gravedo.de>
+ *
+ * libsamsung-ipc is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libsamsung-ipc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libsamsung-ipc.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef SAMSUNGIPC_H_
+#define SAMSUNGIPC_H_
+
+#include <radio.h>
+
+#endif
+
+// vim:ts=4:sw=4:expandtab
