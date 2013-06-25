b8eb72bed49a0353ca8d6fe2dc0d051d482c5d0e
Paul Kocialkowski <contact@paulk.fr>
2013-06-25 19:59:20 +0200

    modemctrl: Update Copyright block for better accuracy
    Change-Id: I0ddd3a6b32920033c3d70f046aea3674f94fa75e
    Signed-off-by: Paul Kocialkowski <contact@paulk.fr>

--- tools/modemctrl.c
+++ tools/modemctrl.c
@@ -1,8 +1,8 @@
-/**
+/*
  * This file is part of libsamsung-ipc.
  *
- * Copyright (C) 2010-2011 Joerie de Gram <j.de.gram@gmail.com>
- *               2011 Simon Busch <morphis@gravedo.de>
+ * Copyright (C) 2011 Simon Busch <morphis@gravedo.de>
+ * Copyright (C) 2011 Paul Kocialkowsk <contact@paulk.fr>
  *
  * libsamsung-ipc is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,6 +16,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with libsamsung-ipc.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 #include <stdlib.h>
