ed1c306e084552c85da08e2fd242255853418d91
Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>
2020-02-06 22:22:44 +0100

    devices: i9300: Reorder includes alphabetically
    Acked-by: Fil Lupin <fillupin@protonmail.com>
    Signed-off-by: Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>

--- samsung-ipc/devices/i9300/i9300.c
+++ samsung-ipc/devices/i9300/i9300.c
@@ -17,19 +17,19 @@
  * along with libsamsung-ipc.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <fcntl.h>
 #include <sys/mman.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-#include <samsung-ipc.h>
 #include <ipc.h>
+#include <samsung-ipc.h>
 
+#include "i9300.h"
 #include "xmm626.h"
 #include "xmm626_hsic.h"
 #include "xmm626_sec_modem.h"
-#include "i9300.h"
 
 int i9300_boot(struct ipc_client *client)
 {
