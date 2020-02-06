b14de741d370ddf321be91c1d7ba5660fd52eb9e
Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>
2020-02-06 22:22:45 +0100

    devices: n7100: Reorder includes alphabetically
    Acked-by: Fil Lupin <fillupin@protonmail.com>
    Signed-off-by: Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>

--- samsung-ipc/devices/n7100/n7100.c
+++ samsung-ipc/devices/n7100/n7100.c
@@ -17,19 +17,19 @@
  * along with libsamsung-ipc.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <fcntl.h>
 #include <sys/mman.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-#include <samsung-ipc.h>
 #include <ipc.h>
+#include <samsung-ipc.h>
 
+#include "n7100.h"
 #include "xmm626.h"
 #include "xmm626_hsic.h"
 #include "xmm626_sec_modem.h"
-#include "n7100.h"
 
 int n7100_boot(struct ipc_client *client)
 {
