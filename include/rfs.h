8801cb486cebbef59b885c76f7744f334c1daa9a
Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>
2020-04-04 19:11:17 +0200

    includes: rfs.h: switch to Linux code style
    This contains no functional changes.
    
    Signed-off-by: Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>
    Reviewed-by: Joonas Kylmälä <joonas.kylmala@iki.fi>

--- include/rfs.h
+++ include/rfs.h
@@ -26,41 +26,41 @@
  * Commands
  */
 
-#define IPC_RFS_NV_READ_ITEM                                    0x4201
-#define IPC_RFS_NV_WRITE_ITEM                                   0x4202
+#define IPC_RFS_NV_READ_ITEM				0x4201
+#define IPC_RFS_NV_WRITE_ITEM				0x4202
 
 /*
  * Values
  */
 
-#define NV_DATA_SECRET                          "Samsung_Android_RIL"
-#define NV_DATA_SIZE                                            0x200000
-#define NV_DATA_CHUNK_SIZE                                      0x1000
+#define NV_DATA_SECRET					"Samsung_Android_RIL"
+#define NV_DATA_SIZE					0x200000
+#define NV_DATA_CHUNK_SIZE				0x1000
 
 /*
  * Structures
  */
 
 struct ipc_rfs_nv_read_item_request_data {
-    unsigned int offset;
-    unsigned int length;
+	unsigned int offset;
+	unsigned int length;
 } __attribute__((__packed__));
 
 struct ipc_rfs_nv_read_item_response_header {
-    unsigned char confirm;
-    unsigned int offset;
-    unsigned int length;
+	unsigned char confirm;
+	unsigned int offset;
+	unsigned int length;
 } __attribute__((__packed__));
 
 struct ipc_rfs_nv_write_item_request_header {
-    unsigned int offset;
-    unsigned int length;
+	unsigned int offset;
+	unsigned int length;
 } __attribute__((__packed__));
 
 struct ipc_rfs_nv_write_item_response_data {
-    unsigned char confirm;
-    unsigned int offset;
-    unsigned int length;
+	unsigned char confirm;
+	unsigned int offset;
+	unsigned int length;
 } __attribute__((__packed__));
 
 /*
@@ -80,16 +80,16 @@ int ipc_nv_data_backup(struct ipc_client *client);
 int ipc_nv_data_restore(struct ipc_client *client);
 void *ipc_nv_data_load(struct ipc_client *client);
 void *ipc_nv_data_read(struct ipc_client *client, size_t size,
-    unsigned int offset);
+		       unsigned int offset);
 int ipc_nv_data_write(struct ipc_client *client, const void *data, size_t size,
-    unsigned int offset);
-size_t ipc_rfs_nv_data_item_size_setup(struct ipc_rfs_nv_read_item_response_header *header,
-    const void *nv_data, size_t nv_size);
-void *ipc_rfs_nv_read_item_setup(struct ipc_rfs_nv_read_item_response_header *header,
-    const void *nv_data, size_t nv_size);
+		      unsigned int offset);
+size_t ipc_rfs_nv_data_item_size_setup(
+	struct ipc_rfs_nv_read_item_response_header *header,
+	const void *nv_data, size_t nv_size);
+void *ipc_rfs_nv_read_item_setup(
+	struct ipc_rfs_nv_read_item_response_header *header,
+	const void *nv_data, size_t nv_size);
 size_t ipc_rfs_nv_write_item_size_extract(const void *data, size_t size);
 void *ipc_rfs_nv_write_item_extract(const void *data, size_t size);
 
-#endif
-
-// vim:ts=4:sw=4:expandtab
+#endif /* __SAMSUNG_IPC_RFS_H__ */
