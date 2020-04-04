3d2824f68d549273efaa1b3961d9c1bd48c1a2e7
Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>
2020-04-04 19:11:17 +0200

    devices: n5100: switch to Linux code style
    This contains no functional changes.
    
    Signed-off-by: Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>
    Reviewed-by: Joonas Kylmälä <joonas.kylmala@iki.fi>

--- samsung-ipc/devices/n5100/n5100.h
+++ samsung-ipc/devices/n5100/n5100.h
@@ -21,22 +21,23 @@
 #ifndef __N5100_H__
 #define __N5100_H__
 
-#define N5100_MODEM_IMAGE_SIZE                                  0x1000000
-#define N5100_PSI_OFFSET                                        0x1000
-#define N5100_PSI_SIZE                                          0xE000
-#define N5100_EBL_OFFSET                                        0xF000
-#define N5100_EBL_SIZE                                          0x19000
-#define N5100_SEC_START_OFFSET                                  0x9FF800
-#define N5100_SEC_START_SIZE                                    0x800
-#define N5100_FIRMWARE_OFFSET                                   0x28000
-#define N5100_FIRMWARE_SIZE                                     0x9D7800
-#define N5100_NV_DATA_OFFSET                                    0xA00000
-#define N5100_NV_DATA_SIZE                                      0x200000
+#define N5100_MODEM_IMAGE_SIZE		0x1000000
+#define N5100_PSI_OFFSET		0x1000
+#define N5100_PSI_SIZE			0xE000
+#define N5100_EBL_OFFSET		0xF000
+#define N5100_EBL_SIZE			0x19000
+#define N5100_SEC_START_OFFSET		0x9FF800
+#define N5100_SEC_START_SIZE		0x800
+#define N5100_FIRMWARE_OFFSET		0x28000
+#define N5100_FIRMWARE_SIZE		0x9D7800
+#define N5100_NV_DATA_OFFSET		0xA00000
+#define N5100_NV_DATA_SIZE		0x200000
 
-#define N5100_MODEM_IMAGE_DEVICE                "/dev/block/platform/dw_mmc/by-name/RADIO"
+#define N5100_MODEM_IMAGE_DEVICE	\
+	"/dev/block/platform/dw_mmc/by-name/RADIO"
 
 struct n5100_transport_data {
-    int fd;
+	int fd;
 };
 
 extern struct ipc_client_ops n5100_fmt_ops;
@@ -45,6 +46,4 @@ extern struct ipc_client_handlers n5100_handlers;
 extern struct ipc_client_gprs_specs n5100_gprs_specs;
 extern struct ipc_client_nv_data_specs n5100_nv_data_specs;
 
-#endif
-
-// vim:ts=4:sw=4:expandtab
+#endif /* __N5100_H__ */
